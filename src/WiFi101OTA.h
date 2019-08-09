104617302c02ec88acd0e387dc88adb1e22c9162
Martino Facchin <m.facchin@arduino.cc>
2019-08-09 09:23:27 +0200

    Add Nano33IoT and MKRVidor as targets

--- src/WiFi101OTA.h
+++ src/WiFi101OTA.h
@@ -33,6 +33,12 @@
 #elif defined(ARDUINO_SAMD_MKRWIFI1010)
   #include "WiFiNINA.h"
   #define BOARD "mkrwifi1010"
+#elif defined(ARDUINO_SAMD_NANO_33_IOT)
+  #include "WiFiNINA.h"
+  #define BOARD "nano_33_iot"
+#elif defined(ARDUINO_SAMD_MKRVIDOR4000)
+  #include "WiFiNINA.h"
+  #define BOARD "mkrvidor4000"
 #elif defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined(ARDUINO_MEGAAVR_UNO_WIFI_REV2)
   #include "WiFiNINA.h"
   #define BOARD "uno2018"
